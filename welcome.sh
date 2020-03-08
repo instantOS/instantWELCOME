19cf084b4f90dbc1af814e91661022a99eb1f4d4
paperbenni <paperbenni@gmail.com>
2020-03-08 14:48:04 +0100

    fix window name detection

--- welcome.sh
+++ welcome.sh
@@ -9,13 +9,12 @@ fi
 
 while :; do
     sleep 0.2
-    WINDOWTITLE=$(xdotool getactivewindow getwindowname)
-    if grep -iq 'welcome' <<<"$WINDOWTITLE"; then
+    WINDOWTITLE="$(xdotool getactivewindow getwindowname)"
+    if grep -iq 'welcome' <<<"$WINDOWTITLE.."; then
         break
     fi
 done
 
-xdotool getactivewindow getwindowname
 xdotool key super+shift+space
 sleep 0.2
 xdotool key super+alt+w
